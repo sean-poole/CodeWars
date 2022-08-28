8784a2cd6bdd4b52abb6b5ddec85bc4764d5c98d
Sean Poole <spoole5289@gmail.com>
2022-08-28 08:45:35 -0700

    Added -Opposite Number- CodeWars challenge.

---
+++ oppositeNumber.js
@@ -0,0 +1,14 @@
+/* Opposite Number */
+
+/*
+Given an integer or a floating-point number, find its opposite.
+
+Examples:
+1: -1
+14 = -14
+-34 = 34
+*/
+
+function opposite(number) {
+    return -number;
+}
