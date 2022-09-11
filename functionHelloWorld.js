a067245dfa1a55518de4f2064eeeb01a12df77f9
Sean Poole <spoole5289@gmail.com>
2022-09-11 08:22:08 -0700

    Added -Function Hello World- CodeWars challenge.

---
+++ functionHelloWorld.js
@@ -0,0 +1,12 @@
+/* Function 1 - hello world */
+
+/*
+Description:
+Make a simple function called greet that returns the most-famous "hello world!".
+*/
+
+function greet() {
+    return "hello world!";
+}
+
+// const greet = () => "hello world!";
