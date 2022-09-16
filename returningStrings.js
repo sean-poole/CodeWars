ed22ebac91ecdd92f04a7298903da55b83caa8f6
Sean Poole <spoole5289@gmail.com>
2022-09-16 12:14:01 -0700

    Added -Returning Strings- CodeWars challenge.

---
+++ returningStrings.js
@@ -0,0 +1,11 @@
+/* Returning Strings */
+
+/*
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+*/
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
+
+// const greet = name => `Hello, ${name} how are you doing today?`;
