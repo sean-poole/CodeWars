44564435ffb6e3c918b40df0b3091df8d64eda08
Sean Poole <spoole5289@gmail.com>
2022-09-19 11:11:11 -0700

    Added -Code Under Pressure #1- CodeWars challenge.

---
+++ codeUnderPressure01.js
@@ -0,0 +1,11 @@
+/* Code Under Pressure #1 */
+
+/*
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i) {
+    return i * 2;
+}
+
+// const doubleInteger = i => i * 2;
