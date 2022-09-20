317d1764183f6baad30a43cfe613c142ce7fbc30
Sean Poole <spoole5289@gmail.com>
2022-09-20 10:05:22 -0700

    Added -Lost Without A Map- CodeWars challenge.

---
+++ lostWithoutAMap.js
@@ -0,0 +1,15 @@
+/* Lost Without A Map */
+
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+function maps(x) {
+    return x.map(e => e * 2);
+}
+
+// maps = x => x.map(e => e * 2);
