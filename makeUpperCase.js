b3572c9c0ebfed22e1b28ee6ea9eeb573209faee
Sean Poole <spoole5289@gmail.com>
2022-09-26 07:41:06 -0700

    Added -Make UpperCase- Codewars challenge.

---
+++ makeUpperCase.js
@@ -0,0 +1,16 @@
+/* Make UpperCase */
+
+/*
+Write a function which converts the input string to uppercase.
+*/
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+// Alternate solution: 
+// const makeUpperCase = str => str.toUpperCase();
+
+// Test: 
+// word = "hello"
+// console.log(makeUpperCase(word))
