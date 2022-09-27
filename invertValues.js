78a88c0f0b13e0bd25e249d87f6f526e8f0c293f
Sean Poole <spoole5289@gmail.com>
2022-09-27 07:47:04 -0700

    Added -Invert Values- CodeWars challenge.

---
+++ invertValues.js
@@ -0,0 +1,28 @@
+/* Invert Values */
+
+/*
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+Examples: 
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+
+You can assume that all values are integers. Do not mutate the input array/list.
+*/
+
+function invert(array) {
+    return array.map(x => x * -1);
+}
+
+// Alternate solution:
+// const invert = array => array.map(x => x * -1);
+
+// Test: 
+// test1 = [1, 2, 3, 4, 5]
+// test2 = [1, -2, 3, -4, 5]
+// test3 = []
+
+// console.log(invert(test1))
+// console.log(invert(test2))
+// console.log(invert(test3))
