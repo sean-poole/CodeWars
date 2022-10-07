904e3f92215d43a5e71ddac1ccb12e0825a319b7
Sean Poole <spoole5289@gmail.com>
2022-10-07 08:25:41 -0700

    Added -Reversed Sequence- CodeWars challenge.

---
+++ reversedSequence.js
@@ -0,0 +1,19 @@
+/* Reversed Sequence */
+
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+const reverseSeq = n => {
+    let a = []
+    for (n; n > 0; n--) {
+        a.push(n)
+    }
+    return a;
+}
+
+// Test: 
+testNum = 5
+console.log(reverseSeq(testNum))
