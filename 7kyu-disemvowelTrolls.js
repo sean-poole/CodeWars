2ac775245ec48cff52f8b5b4ef884ce547fdd576
Sean Poole <spoole5289@gmail.com>
2022-10-13 08:14:49 -0700

    Added -Disemvowel Trolls- CodeWars challenge.

---
+++ 7kyu-disemvowelTrolls.js
@@ -0,0 +1,23 @@
+/* Disemvowel Trolls */
+
+/*
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+function disemvowel(str) {
+    return str.replace(/[aeiouAEIOU]/g, "");
+}
+
+// Test: 
+test1 = "This website is for losers LOL!"
+test2 = "No offense but,\nYour writing is among the worst I've ever read"
+console.log(disemvowel(test1))
+console.log(disemvowel(test2))
