0912abddc6fe75a99fdca318113e945cda920929
Sean Poole <spoole5289@gmail.com>
2022-10-17 13:12:53 -0700

    Removed console.log test display.

--- transportationOnVacation.js
+++ transportationOnVacation.js
@@ -15,13 +15,13 @@ function rentalCarCost(d) {
 }
 
 // Test: 
-console.log(rentalCarCost(1))
-console.log(rentalCarCost(2))
-console.log(rentalCarCost(3))
-console.log(rentalCarCost(4))
-console.log(rentalCarCost(5))
-console.log(rentalCarCost(6))
-console.log(rentalCarCost(7))
-console.log(rentalCarCost(8))
-console.log(rentalCarCost(9))
-console.log(rentalCarCost(10))
+// console.log(rentalCarCost(1))
+// console.log(rentalCarCost(2))
+// console.log(rentalCarCost(3))
+// console.log(rentalCarCost(4))
+// console.log(rentalCarCost(5))
+// console.log(rentalCarCost(6))
+// console.log(rentalCarCost(7))
+// console.log(rentalCarCost(8))
+// console.log(rentalCarCost(9))
+// console.log(rentalCarCost(10))
