b711643f958207777db5f0db17c1a8656bccc0b0
Sean Poole <spoole5289@gmail.com>
2022-10-19 07:50:48 -0700

    Added -Grasshopper Personalized Message- CodeWars challenge.

---
+++ grasshopperPersonalizedMessage.js
@@ -0,0 +1,23 @@
+/* Grasshopper - Personalized Message */
+
+/*
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	                    return
+name equals owner	        'Hello boss'
+otherwise	                'Hello guest'
+*/
+
+const greet = (name, owner) => name === owner ? "Hello boss" : "Hello guest";
+
+// Test: 
+testName1 = "Daniel"
+testBoss1 = "Daniel"
+
+testName2 = "Greg"
+testBoss2 = "Daniel"
+
+console.log(greet(testName1, testBoss1))
+console.log(greet(testName2, testBoss2))
