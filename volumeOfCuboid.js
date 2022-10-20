40ff25c62e6027bf43aa023606f205a2028a3e37
Sean Poole <spoole5289@gmail.com>
2022-10-20 09:32:10 -0700

    Added -Volume Of A Cuboid- CodeWars challenge.

---
+++ volumeOfCuboid.js
@@ -0,0 +1,11 @@
+/* Volume of a Cuboid */
+
+/*
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+*/
+
+const volume = (l, w, h) => l * w * h;
+
+// Test: 
+console.log(volume(1, 2, 2))
+console.log(volume(6, 2, 5))
