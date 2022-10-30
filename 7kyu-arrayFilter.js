96aaf2a8adc9362d1b4d463c8a963ea7e99bea68
Sean Poole <spoole5289@gmail.com>
2022-10-30 06:56:49 -0700

    Removed console.log test display.

--- 7kyu-arrayFilter.js
+++ 7kyu-arrayFilter.js
@@ -10,8 +10,8 @@ getEvenNumbers([2,4,5,6]) // should == [2,4,6]
 const getEvenNumbers = arr => arr.filter(x => x % 2 == 0);
 
 // Test: 
-console.log(getEvenNumbers([1,2,3,6,8,10]))
-console.log(getEvenNumbers([1, 2]))
-console.log(getEvenNumbers([12, 14, 15]))
-console.log(getEvenNumbers([13, 15]))
-console.log(getEvenNumbers([1, 3, 9]))
+// console.log(getEvenNumbers([1,2,3,6,8,10]))
+// console.log(getEvenNumbers([1, 2]))
+// console.log(getEvenNumbers([12, 14, 15]))
+// console.log(getEvenNumbers([13, 15]))
+// console.log(getEvenNumbers([1, 3, 9]))
