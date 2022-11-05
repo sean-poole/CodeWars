ae72bada8764e4e1a5fe80c180ebb86c7aa0303b
Sean Poole <spoole5289@gmail.com>
2022-11-05 09:33:27 -0700

    Added -Exes and Ohs- CodeWars challenge.

--- 7kyu-exesAndOhs.js
+++ 7kyu-exesAndOhs.js
@@ -4,6 +4,7 @@
 Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
 Examples input/output:
+
 XO("ooxx") => true
 XO("xooxx") => false
 XO("ooxXm") => true
