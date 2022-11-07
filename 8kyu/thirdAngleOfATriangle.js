ad0db9979e7abee96325ef0aea4db0c03df032b0
Sean Poole <spoole5289@gmail.com>
2022-11-07 10:14:43 -0800

    Added -Third Angle Of A Triangle- CodeWars challenge.

---
+++ 8kyu/thirdAngleOfATriangle.js
@@ -0,0 +1,17 @@
+/* Third Angle of the Triangle */
+
+/*
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+*/
+
+const otherAngle = (a, b) => 180 - a - b;
+
+// Test: 
+// console.log(otherAngle(30, 60))
+// console.log(otherAngle(60, 60))
+// console.log(otherAngle(43, 78))
+// console.log(otherAngle(10, 20))
