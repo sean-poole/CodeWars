31058c67b9ba85227721b956fd762c43431ab4d5
Sean Poole <spoole5289@gmail.com>
2022-11-08 10:19:57 -0800

    Removed console.log test display.

--- 6kyu/whoLikesIt.js
+++ 6kyu/whoLikesIt.js
@@ -13,40 +13,40 @@ Implement the function which takes an array containing the names of people that 
 Note: For 4 or more names, the number in "and 2 others" simply increases.
 */
 
-// function likes(names) {
-//     if (names.length == 0) {
-//         return `no one likes this`
-//     } else if (names.length == 1) {
-//         return `${names[0]} likes this`
-//     } else if (names.length == 2) {
-//         return `${names[0]} and ${names[1]} like this`
-//     } else if (names.length == 3) {
-//         return `${names[0]}, ${names[1]} and ${names[2]} like this`
-//     } else {
-//         return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
-//     }
-// }
-
-// Alternate solution: 
 function likes(names) {
-    names = names || [];
-    switch(names.length) {
-        case 0: return `no one likes this`;
-            break;
-        case 1: return `${names[0]} likes this`;
-            break;
-        case 2: return `${names[0]} and ${names[1]} like this`;
-            break;
-        case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-            break;
-        default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
-            break;
+    if (names.length == 0) {
+        return `no one likes this`
+    } else if (names.length == 1) {
+        return `${names[0]} likes this`
+    } else if (names.length == 2) {
+        return `${names[0]} and ${names[1]} like this`
+    } else if (names.length == 3) {
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else {
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
     }
 }
 
+// Alternate solution: 
+// function likes(names) {
+//     names = names || [];
+//     switch(names.length) {
+//         case 0: return `no one likes this`;
+//             break;
+//         case 1: return `${names[0]} likes this`;
+//             break;
+//         case 2: return `${names[0]} and ${names[1]} like this`;
+//             break;
+//         case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//             break;
+//         default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+//             break;
+//     }
+// }
+
 // Test: 
-console.log(likes([]))
-console.log(likes(["Peter"] ))
-console.log(likes(["Jacob", "Alex"] ))
-console.log(likes(["Max", "John", "Mark"] ))
-console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']))
+// console.log(likes([]))
+// console.log(likes(["Peter"] ))
+// console.log(likes(["Jacob", "Alex"] ))
+// console.log(likes(["Max", "John", "Mark"] ))
+// console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']))
