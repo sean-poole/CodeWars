bb1a51b1a61f2e776b70ac7d0d41624ad4d5fdf0
Sean Poole <spoole5289@gmail.com>
2022-11-10 08:53:48 -0800

    Removed console.log test display.

--- 7kyu/sumTwoLowestPositiveIntegers.js
+++ 7kyu/sumTwoLowestPositiveIntegers.js
@@ -17,8 +17,8 @@ function sumTwoSmallestNumbers(numbers) {
 // const sumTwoSmallestNumbers = numbers => numbers.sort((a, b) => a - b).slice(0, 2).reduce((acc, c) => acc += c, 0);
 
 // Test: 
-console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
-console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
-console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]))
-console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]))
-console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]))
+// console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
+// console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
+// console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]))
+// console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]))
+// console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]))
