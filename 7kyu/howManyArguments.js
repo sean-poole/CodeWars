2f480f0df678a246a54e2bbc0106459000e3db22
Sean Poole <spoole5289@gmail.com>
2022-11-27 10:18:05 -0800

    Added -How Many Arguments- CodeWars challenge.

---
+++ 7kyu/howManyArguments.js
@@ -0,0 +1,14 @@
+/* How Many Arguments */
+
+/*
+args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+*/
+
+const args_count = (...args) => args.length;
+
+// Test: 
+// console.log(args_count(1, 2))
+// console.log(args_count())
+// console.log(args_count("A", "B", "C"))
+// console.log(args_count(["foo", "bar"]))
