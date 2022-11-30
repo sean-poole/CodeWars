77e15183497a7b3bd7de9dcbd86eda10fc3d0ca4
Sean Poole <spoole5289@gmail.com>
2022-11-30 10:45:39 -0800

    Added -Sum of a Sequence- CodeWars challenge.

---
+++ 7kyu/sumOfASequence.js
@@ -0,0 +1,29 @@
+/* Sum Of A Sequence */
+
+/*
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+*/
+
+function sequenceSum(begin, end, step) {
+    let result = 0;
+    for (begin; begin <= end; begin += step) {
+        result += begin;
+    }
+    return result;
+}
+
+// Test: 
+// console.log(sequenceSum(2, 6, 2))
+// console.log(sequenceSum(1, 5, 1))
+// console.log(sequenceSum(1, 5, 3))
