753102afdded81f6188fa61116061b6bbe7cd42e
Sean Poole <spoole5289@gmail.com>
2022-12-06 09:40:17 -0800

    Added -Sum of Angles- CodeWars challenge.

---
+++ 7kyu/sumOfAngles.js
@@ -0,0 +1,12 @@
+/* Sum Of Angles */
+
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+*/
+
+const angle = n => (n - 2) * 180;
+
+// Test: 
+// console.log(angle(3))
+// console.log(angle(4))
