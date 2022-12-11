854a701d584c2d76289b1d24c442d998173acc3e
Sean Poole <spoole5289@gmail.com>
2022-12-11 10:25:06 -0800

    Added -Function 2: Squaring an Argument- CodeWars challenge.

---
+++ 8kyu/function2SquaringAnArgument.js
@@ -0,0 +1,11 @@
+/* Function 2 - Squaring An Argument */
+
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+const square = n => n**2;
+
+// Test: 
+console.log(square(3))
