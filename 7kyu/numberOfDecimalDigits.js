b85092ff69abea59c0e6cc746d58b14c8b991222
Sean Poole <spoole5289@gmail.com>
2022-12-13 10:31:43 -0800

    Added -Number of Decimal Digits- CodeWars challenge.

---
+++ 7kyu/numberOfDecimalDigits.js
@@ -0,0 +1,14 @@
+/* Number Of Decimal Digits */
+
+/*
+Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid.
+*/
+
+const digits = n => String(n).length;
+
+// Test: 
+// console.log(digits(5))
+// console.log(digits(12345))
+// console.log(digits(9876543210))
