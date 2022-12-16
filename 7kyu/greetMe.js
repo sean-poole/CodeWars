e591cdd03f45848e132377dbcfc6074e828a3b32
Sean Poole <spoole5289@gmail.com>
2022-12-16 10:32:38 -0800

    Added -Greet Me- CodeWars challenge.

---
+++ 7kyu/greetMe.js
@@ -0,0 +1,15 @@
+/* Greet Me */
+
+/*
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+*/
+
+const greet = name => `Hello ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`;
+
+// Test: 
+// console.log(greet("riley"))
+// console.log(greet("JACK"))
