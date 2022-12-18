016f276ea0d0a17a051f055537e12ae7fca80218
Sean Poole <spoole5289@gmail.com>
2022-12-18 10:09:47 -0800

    Added -Alternating Case- CodeWars challenge.

---
+++ 8kyu/alternatingCase.js
@@ -0,0 +1,29 @@
+/* Alternating Case */
+
+/*
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+*/
+
+function toAlternatingCase(str) {
+    return str.slice().split("").map(e => e === e.toLowerCase() ? e.toUpperCase() : e.toLowerCase()).join("");
+}
+
+// Test: 
+console.log(toAlternatingCase("hello world"))
+console.log(toAlternatingCase("HELLO WORLD"))
+console.log(toAlternatingCase("hello WORLD"))
+console.log(toAlternatingCase("HeLLo WoRLD"))
+console.log(toAlternatingCase("12345"))
+console.log(toAlternatingCase("1a2b3c4d5e"))
+console.log(toAlternatingCase("String.prototype.toAlternatingCase"))
+console.log(toAlternatingCase("Hello World"))
