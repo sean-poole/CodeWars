a246a9d39c291997e3a6c2f31b96252411b28864
Sean Poole <spoole5289@gmail.com>
2022-12-22 10:17:50 -0800

    Added -Sort Arrays- CodeWars challenge.

---
+++ 7kyu/sortArrays.js
@@ -0,0 +1,10 @@
+/* Sort Arrays */
+
+/*
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+const sortme = names => names.sort();
+
+// Test: 
+// console.log(sortme(['one', 'two', 'three']))
