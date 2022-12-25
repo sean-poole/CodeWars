b4413d7ab9b7aa035c4b75f65173a12f8ae42276
Sean Poole <spoole5289@gmail.com>
2022-12-25 10:31:21 -0800

    Added -Is It Even?- CodeWars challenge.

---
+++ 8kyu/isItEven.js
@@ -0,0 +1,18 @@
+/* Is It Even? */
+
+/*
+In this Kata we are passing a number (n) into a function.
+Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false.
+Numbers may be positive or negative, integers or floats.
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+const testEven = n => n % 2 === 0;
+
+// Test: 
+// console.log(testEven(0))
+// console.log(testEven(0.5))
+// console.log(testEven(1))
+// console.log(testEven(2))
+// console.log(testEven(-4))
