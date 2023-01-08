b240e9a0ff2235971c02b26aac6a2bcdd9a519b8
Sean Poole <spoole5289@gmail.com>
2023-01-08 10:47:24 -0800

    Added -Add Property to Every Object in Array- CodeWars challenge.

---
+++ 7kyu/addPropertyToEveryObjectInArray.js
@@ -0,0 +1,48 @@
+/* Add Property To Every Object In Array */
+
+/*
+Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+
+For example:
+
+var questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+
+After adding the property the result should be:
+var questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0,
+    usersAnswer: null
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+    corAnswer: 0,
+    usersAnswer: null
+}];
+*/
+
+questions.forEach(obj => obj.usersAnswer = null);
+
+// Test: 
+// let questions = [
+//     {
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0
+//     },
+//     {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+//     corAnswer: 0
+//     }
+// ];
+
+// console.log(addProperty(questions));
