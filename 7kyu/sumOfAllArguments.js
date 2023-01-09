57a815beab26406b9bff03353aa6ee2c47deab75
Sean Poole <spoole5289@gmail.com>
2023-01-09 12:07:54 -0800

    Added -Sum of all Arguments- CodeWars challenge.

---
+++ 7kyu/sumOfAllArguments.js
@@ -0,0 +1,19 @@
+/* Sum Of All Arguments */
+
+/*
+Write a function that finds the sum of all its arguments.
+
+eg:
+sum(1, 2, 3) // => 6
+sum(8, 2) // => 10
+sum(1, 2, 3, 4, 5) // => 15
+*/
+
+const sum = (...args) => [...args].reduce((acc, c) => acc += c, 0);
+
+// Test: 
+// console.log(sum(1))
+// console.log(sum(1, 2))
+// console.log(sum(5, 7, 9))
+// console.log(sum(12, 1, 1, 1, 1))
+// console.log(sum(12, 1, 1, 1, 1, 1, 1))
