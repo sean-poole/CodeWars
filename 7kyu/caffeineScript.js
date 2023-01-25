27353993c9723a278c6a488bc9626ce3be610f0f
Sean Poole <spoole5289@gmail.com>
2023-01-25 09:12:10 -0800

    Added -Caffeine Script- CodeWars challenge.

---
+++ 7kyu/caffeineScript.js
@@ -0,0 +1,39 @@
+/* Caffeine Script */
+
+/*
+Complete the function which takes a non-zero integer as its argument.
+If the integer is divisible by 3, return the string "Java".
+If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+If none of the condition is true, return the string "mocha_missing!"
+
+Examples: 
+1   -->  "mocha_missing!"
+3   -->  "Java"
+6   -->  "JavaScript"
+12  -->  "CoffeeScript"
+*/
+
+function caffeineBuzz(n) {
+    let result = "";
+
+    if (n % 12 === 0) {
+        result = "Coffee";
+    } else if (n % 3 === 0) {
+        result = "Java";
+    } else {
+        result = "mocha_missing!";
+    }
+
+    if ((result === "Coffee" || result === "Java") && n % 2 === 0) {
+        result += "Script";
+    }
+
+    return result;
+}
+
+// Test: 
+// console.log(caffeineBuzz(1))
+// console.log(caffeineBuzz(3))
+// console.log(caffeineBuzz(6))
+// console.log(caffeineBuzz(12))
