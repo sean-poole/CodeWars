d2f98c4e6eaa102209503457ab242dba771fcf9b
Sean Poole <spoole5289@gmail.com>
2023-01-25 10:00:06 -0800

    Added -Longest Palindrome- CodeWars challenge.

--- 6kyu/longestPalindrome.js
+++ 6kyu/longestPalindrome.js
@@ -13,46 +13,35 @@ Example:
 "" -> 0
 */
 
-// function getPalindrome(left, right, s) {
-//     while (left >= 0 && right < s.length) {
-//         if (s[left] !== s[right]) break;
-//         left--;
-//         right++;
-//     }
-
-//     return [left + 1, right];
-// }
-
-// function longestPalindrome(s) {
-//     let max = [0, 1];
-
-//     for (let i = 0; i < s.length; i++) {
-//         let even = getPalindrome(i - 1, i, s);
-//         let odd = getPalindrome(i - 1, i + 1, s);
-//         let current = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
-
-//         max = max[1] - max[0] > current[1] - current[0] ? max : current;
-//     }
-
-//     return s.slice(max[0], max[1]).length;
-// }
-
-// Alternate solution: 
-function longestPalindrome(s) {
-    if (!s) return 0;
-    for (let c = s.length; c > 0; c--) {
-        for (let i = 0; i <= s.length - c; i++) {
-            let check = s.substring(i, c);
-            if (check === check.split("").reverse().join("")) return c;
-        }
+function getPalindrome(left, right, s) {
+    while (left >= 0 && right < s.length) {
+        if (s[left] !== s[right]) break;
+        left--;
+        right++;
     }
+
+    return [left + 1, right];
+}
+
+function longestPalindrome(s) {
+    let max = [0, 1];
+
+    for (let i = 0; i < s.length; i++) {
+        let even = getPalindrome(i - 1, i, s);
+        let odd = getPalindrome(i - 1, i + 1, s);
+        let current = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+
+        max = max[1] - max[0] > current[1] - current[0] ? max : current;
+    }
+
+    return s.slice(max[0], max[1]).length;
 }
 
 // Test: 
-console.log(longestPalindrome("a"))
-console.log(longestPalindrome("aa"))
-console.log(longestPalindrome("baa"))
-console.log(longestPalindrome("aab"))
-console.log(longestPalindrome("zyabyz"))
-console.log(longestPalindrome("baabcd"))
-console.log(longestPalindrome("baablkj12345432133d"))
+// console.log(longestPalindrome("a"))
+// console.log(longestPalindrome("aa"))
+// console.log(longestPalindrome("baa"))
+// console.log(longestPalindrome("aab"))
+// console.log(longestPalindrome("zyabyz"))
+// console.log(longestPalindrome("baabcd"))
+// console.log(longestPalindrome("baablkj12345432133d"))
