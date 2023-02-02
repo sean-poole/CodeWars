560b3098f73275f3ead22eafd03e3b45b0954de4
Sean Poole <spoole5289@gmail.com>
2023-02-02 10:42:04 -0800

    Added -Vowel One- CodeWars challenge.

---
+++ 7kyu/vowelOne.js
@@ -0,0 +1,16 @@
+/* Vowel One */
+
+/*
+Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+Examples:
+> vowelOne( "abceios" ) // "1001110"
+> vowelOne( "aeiou, abc" ) // "1111100100"
+*/
+
+const vowelOne = s => s.split("").map(c => "aeiou".includes(c.toLowerCase()) ? 1 : 0).join("");
+
+// Test: 
+// console.log(vowelOne("vowelOne"))
+// console.log(vowelOne("123, arou"))
