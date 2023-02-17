425366f3a9d4f5ee1cfff4170d85c8fb8ad1f6f9
Sean Poole <spoole5289@gmail.com>
2023-02-16 17:35:23 -0800

    Added -Recursion #01: Factorial- CodeWars challenge.

---
+++ 7kyu/recursion01Factorial.js
@@ -0,0 +1,25 @@
+/* Recursion #01 - Factorial */
+
+/*
+This is a kata series that you can only solve using recursion.
+
+##1 - Factorial
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+
+5! = 5 * 4 * 3 * 2 * 1 = 120.
+The value of 0! is 1.
+
+#Your task
+You have to create the function factorial that receives n and returns n!. You have to use recursion.
+*/
+
+function factorial(n) {
+    if (n === 0) return 1;
+    else return n * factorial(n - 1);
+}
+
+// Test: 
+// console.log(factorial(0))
+// console.log(factorial(1))
+// console.log(factorial(2))
+// console.log(factorial(3))
