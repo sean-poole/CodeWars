6340ec97e5d7aaa5dc5b9e43cb9f54801c70b89d
Sean Poole <spoole5289@gmail.com>
2023-03-02 12:56:28 -0800

    Added -Grasshopper: Basic Function Fixer- CodeWars challenge.

---
+++ 8kyu/grasshopperBasicFunctionFixer.js
@@ -0,0 +1,13 @@
+/* Grasshopper - Basic Function Fixer */
+
+/*
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+Can you help me fix the function?
+*/
+
+const addFive = num => num + 5;
+
+// Test: 
+// console.log(addFive(5))
+// console.log(addFive(0))
+// console.log(addFive(-5))
