634cda70260d783b57ba421fe88f4cd891fdea52
Sean Poole <spoole5289@gmail.com>
2023-03-05 12:05:27 -0800

    Added -Get Character From ASCII Value- CodeWars challenge.

---
+++ 8kyu/getCharacterFromASCIIValue.js
@@ -0,0 +1,25 @@
+/* Get Character From ASCII Value */
+
+/*
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+> get_char(65)
+> should return:
+> 'A'
+*/
+
+const getChar = c => String.fromCharCode(c)
+
+// Test: 
+console.log(getChar(55))
+console.log(getChar(56))
+console.log(getChar(57))
+console.log(getChar(58))
+console.log(getChar(59))
+console.log(getChar(60))
+console.log(getChar(61))
+console.log(getChar(62))
+console.log(getChar(63))
+console.log(getChar(64))
+console.log(getChar(65))
