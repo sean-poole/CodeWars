a2a484cb0026fb481378fe5899df03f115c36edb
Sean Poole <spoole5289@gmail.com>
2023-03-31 11:03:01 -0700

    Added -Gravity Flip- CodeWars challenge.

--- 8kyu/gravityFlip.js
+++ 8kyu/gravityFlip.js
@@ -31,7 +31,8 @@ flip('R', [3, 2, 1, 2])         // [1, 2, 2, 3]
 flip('L', [1, 4, 5, 3, 5])     // [5, 5, 4, 3, 1]
 P - 
 function flip(d, a) {
-    // 
+    // If given string === "R", sort the given array in ascending order.
+    // Else, sort the given array in descending order.
 }
 */
 
