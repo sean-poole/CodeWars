61ac1e3edcfca94daff055a3c0b7eb5c4b5d7d3d
Sean Poole <spoole5289@gmail.com>
2023-04-01 10:08:19 -0700

    Added -Human Readable Time- CodeWars challenge.

--- 5kyu/humanReadableTime.js
+++ 5kyu/humanReadableTime.js
@@ -33,13 +33,13 @@ function humanReadable(seconds) {
 }
 
 // Test: 
-console.log(humanReadable(0))
-console.log(humanReadable(59))
-console.log(humanReadable(60))
-console.log(humanReadable(90))
-console.log(humanReadable(3599))
-console.log(humanReadable(3600))
-console.log(humanReadable(45296))
-console.log(humanReadable(86399))
-console.log(humanReadable(86400))
-console.log(humanReadable(359999))
+// console.log(humanReadable(0))
+// console.log(humanReadable(59))
+// console.log(humanReadable(60))
+// console.log(humanReadable(90))
+// console.log(humanReadable(3599))
+// console.log(humanReadable(3600))
+// console.log(humanReadable(45296))
+// console.log(humanReadable(86399))
+// console.log(humanReadable(86400))
+// console.log(humanReadable(359999))
