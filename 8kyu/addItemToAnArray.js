e7cdd898507ed140d8ab3f0d172a1de0a141e2f2
Sean Poole <spoole5289@gmail.com>
2023-04-09 10:39:16 -0700

    Added -Add Item to an Array- CodeWars challenge.

---
+++ 8kyu/addItemToAnArray.js
@@ -0,0 +1,20 @@
+/* Add Item To An Array */
+
+/*
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+/*
+P - Empty array in "websites" variable.
+R - Array containing a string element of "codewars"
+E - websites        // ["codewars"]
+P - Push "codewars" to "websites" array.
+*/
+
+websites = [];
+websites.push("codewars");
