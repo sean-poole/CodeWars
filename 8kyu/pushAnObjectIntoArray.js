8bc65d9e9c68be5414f6e831d0f8311cd03132af
Sean Poole <spoole5289@gmail.com>
2023-05-08 10:56:27 -0700

    Added -Push an Object into Array- CodeWars challenge.

---
+++ 8kyu/pushAnObjectIntoArray.js
@@ -0,0 +1,20 @@
+/* Push An Object Into Array */
+
+/*
+You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+*/
+
+/*
+P - One variable storing an empty array and one push method to add objects to the array.
+R - Make corrections to the given code so it completes the task.
+E - console.log(items)      // [ {a: "b", c: "d"} ]
+P - 
+items = []
+items.push {a: "b", c: "d"}
+*/
+
+let items = [];
+items.push({ a: "b", c: "d" })
+
+// Test: 
+// console.log(items)
