c7c3a6b3e2c1b60fa4d4be2d818e06181adfd4a3
Sean Poole <spoole5289@gmail.com>
2023-05-23 11:17:31 -0700

    Added -Basic Variable Assignment- CodeWars challenge.

---
+++ 8kyu/basicVariableAssignment.js
@@ -0,0 +1,22 @@
+/* Basic Variable Assignment */
+
+/*
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+*/
+
+/*
+P - Fix the code to assign strings to variables.
+R - String of concatenated string variables.
+E - console.log(name)     // "codewa.rs"
+P - Use "=" for assignment, instead of comparison operator "==".
+let a == "code";
+let b == "wa.rs";
+let name == a + b;
+*/
+
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
+
+// Test
+// console.log(name);
