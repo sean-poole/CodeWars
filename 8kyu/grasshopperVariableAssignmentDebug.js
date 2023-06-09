5774b3d02613639828a363d29308e5bda5422ee9
Sean Poole <spoole5289@gmail.com>
2023-06-09 09:10:42 -0700

    Added -Grasshopper: Variable Assignment Debug- CodeWars challenge.

---
+++ 8kyu/grasshopperVariableAssignmentDebug.js
@@ -0,0 +1,27 @@
+/* Grasshopper - Variable Assignment Debug */
+
+/*
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+
+/*
+P - Incorrect variable assignment.
+R - Three variables using the correct operator to assign variables.
+E - a + b     // "devLab"
+P - 
+let a == "dev"
+let b == "Lab"
+let name == a + b
+
+// Change "==" comparison operator to "=" assignment operator.
+*/
+
+let a = "dev";
+let b = "Lab";
+
+let name = a + b;
+
+// Test: 
+// console.log(a)
+// console.log(b)
+// console.log(name)
