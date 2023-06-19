1e01058c5f6dbcd144fdd76d7768102d4c552274
Sean Poole <spoole5289@gmail.com>
2023-06-19 09:58:53 -0700

    Added -Training JS #3: Basic Data Types - String- CodeWars challenge.

---
+++ 8kyu/trainingJS03BasicDataTypesString.js
@@ -0,0 +1,60 @@
+/* Training JS #3: Basic Data Types - Strings */
+
+/*
+In javascript, String is one of basic data types. Use "" or '' to declare a string variable. It can be letters:a,b,c,A,B,C, numbers: 123..., Various symbol:@#$%^&* , Control symbol:\n,\t etc..
+
+String can use operators +, Connect two or more strings together.
+
+Task: 
+misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
+*/
+
+/*
+P - 6 empty funcions. Each function does not take any arguments and refers to the given variables to return a desired output.
+R - 6 completed functions that fulfill the tasks listed within them.
+E - 
+Dad()       // "Dad"
+Bee()       // "Bee"
+banana()    // "banana"
+answer1()   // "yes"
+answer2()   // "no"
+answer3()   // "yes"
+P - 
+const Dad = () => combine variables to output "Dad"
+
+const Bee = () => combine variables to output "Bee"
+
+const banana = () => combine variables to output "banana"
+
+const answer1 = () => return "no"
+
+const answer2 = () => return "no"
+
+const answer3 = () => return "yes"
+*/
+
+let a1="A", a2="a", b1="B", b2="b", c1="C", c2="c", d1="D", d2="d", e1="E", e2="e", n1="N", n2="n";
+
+const Dad = () => d1 + a2 + d2;
+
+const Bee = () => b1 + e2 + e2;;
+
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2;
+
+const answer1 = () => "no";
+
+const answer2 = () => "no";
+
+const answer3 = () => "yes";
+
+// Test: 
+// console.log(Dad())
+// console.log(Bee())
+// console.log(banana())
+// console.log(answer1())
+// console.log(answer2())
+// console.log(answer3())
