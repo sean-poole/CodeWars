227bc392e10e58ac4e2b9e1ca91316df25583f72
Sean Poole <spoole5289@gmail.com>
2023-07-17 08:16:10 -0700

    Added -Honey to the Bee- CodeWars challenge.

---
+++ 7kyu/honeyToTheBee.js
@@ -0,0 +1,52 @@
+/* Honey To The Bee */
+
+/*
+Find out why the amount of honey in the hive is not increasing.
+*/
+
+/*
+P - 
+R - 
+E - 
+bee.capacity        // 11
+hive.pollen         // 100
+bee.unloadPollen()
+hive.pollen         // 111
+P - 
+Bee.prototype.unloadPollen = function() {
+  hive.addPollen(this.capacity);
+}
+// The "hive" property of Bee must be accessed with "this"
+*/
+
+function Bee(capacity, hive) {
+  this.capacity = capacity;
+  this.hive = hive;
+}
+
+function Hive() {
+  this.pollen = 100;
+}
+
+Hive.prototype.addPollen = function(pollen) {
+  this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function() {
+  return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function() {
+  this.hive.addPollen(this.capacity);
+}
+
+// Test: 
+const hive = new Hive();
+const bee = new Bee(11, hive);
+
+console.log(bee.capacity)   // 11
+console.log(hive.pollen)    // 100
+
+bee.unloadPollen();
+
+console.log(hive.pollen)    // 111
