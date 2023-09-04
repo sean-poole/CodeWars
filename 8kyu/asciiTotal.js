1cdd46d7e43ed82476b2c48433148265157f7972
Sean Poole <spoole5289@gmail.com>
2023-09-04 09:03:05 -0700

    Added -ASCII Total- CodeWars challenge.

--- 8kyu/asciiTotal.js
+++ 8kyu/asciiTotal.js
@@ -34,11 +34,11 @@ function uniTotal(str) {
 // const uniTotal = str => [...str].reduce((acc, c) => acc += c.charCodeAt(), 0);
 
 // Test: 
-console.log(uniTotal(""))
-console.log(uniTotal("a"))
-console.log(uniTotal("b"))
-console.log(uniTotal("c"))
-console.log(uniTotal("d"))
-console.log(uniTotal("e"))
-console.log(uniTotal("aaa"))
-console.log(uniTotal("Mary Had A Little Lamb"))
+// console.log(uniTotal(""))
+// console.log(uniTotal("a"))
+// console.log(uniTotal("b"))
+// console.log(uniTotal("c"))
+// console.log(uniTotal("d"))
+// console.log(uniTotal("e"))
+// console.log(uniTotal("aaa"))
+// console.log(uniTotal("Mary Had A Little Lamb"))
