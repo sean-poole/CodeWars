97bd5268a932705cc6ee547722091384e8689964
Sean Poole <spoole5289@gmail.com>
2023-10-15 06:42:02 -0700

    Added -Pirates! Are the Cannons Ready?- CodeWars challenge.

---
+++ 8kyu/piratesAreTheCannonsReady.js
@@ -0,0 +1,44 @@
+/* Pirates! Are The Cannons Ready? */
+
+/*
+Ahoy Matey!
+Welcome to the seven seas.
+You are the captain of a pirate ship.
+You are in battle against the royal navy.
+You have cannons at the ready.... or are they?
+
+Your task is to check if the gunners are loaded and ready, if they are: Fire!
+If they aren't ready: Shiver me timbers!
+Your gunners for each test case are 2, 3 or 4.
+When you check if they are ready their answers are in a dictionary and will either be: aye or nay
+Firing with less than all gunners ready is non-optimum (this is not fire at will, this is fire by the captain's orders or walk the plank, dirty sea-dog!)
+If all answers are 'aye' then Fire! if one or more are 'nay' then Shiver me timbers!
+*/
+
+/*
+P - One argument as an object.
+R - String representing whether all of the entries in the given object are ready.
+E - 
+cannonsReady({'Mike':'aye','Joe':'aye','Johnson':'aye','Peter':'aye'})
+// "Fire!"
+cannonsReady({'Mike':'aye','Joe':'nay','Johnson':'aye','Peter':'aye'})
+// "Shiver me timbers!"
+P - 
+function cannonsReady(gunners) {
+  // Check if all values within the given object are ready.
+  // Return "Fire!" if truthy, otherwise return "Shiver me timbers!".
+}
+*/
+
+function cannonsReady(gunners) {
+  return Object.values(gunners).every(e => e === "aye") 
+    ? "Fire!" 
+    : "Shiver me timbers!";
+}
+
+// Test: 
+let a = {'Mike':'aye','Joe':'aye','Johnson':'aye','Peter':'aye'};
+let b = {'Mike':'aye','Joe':'nay','Johnson':'aye','Peter':'aye'};
+
+// console.log(cannonsReady(a))
+// console.log(cannonsReady(b))
